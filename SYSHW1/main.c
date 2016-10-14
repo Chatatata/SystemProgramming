85f0ba75507209686c8c4af6a0128cd81aba7f34
Ekuklu, B <ekuklu@icloud.com>
2016-10-14 14:12:08 +0300

    Initial Commit

---
+++ SYSHW1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SYSHW1
+//
+//  Created by Buğra Ekuklu on 14.10.2016.
+//  Copyright © 2016 The Digital Warehouse. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
